722263bbe60ef0084adfef86daabd521116d9d52
Kurt Secretario <secretariokurt3219@gmail.com>
2024-07-27 11:53:36 +0800

    add default constructor for the list class

--- src/List.java
+++ src/List.java
@@ -10,6 +10,26 @@ public class List {
         }
     }
 
+    private Node _head, _tail;
+    private int _size;
+
+    // Default constructor
+    public List() {
+        this._head = new Node();
+        this._tail = new Node();
+
+        this._head._next = _tail;
+        this._head._prev = null;
+        this._tail._prev = _head;
+        this._tail._next = null;
+
+        this._size = 0;
+    }
+
+    public List(final List other) {
+        // Copy constructor
+    }
+
     public static void main(String[] args) {
     }
 }
