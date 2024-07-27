b5a137178afee201024f5d5209cefcb187839052
Kurt Secretario <secretariokurt3219@gmail.com>
2024-07-27 13:54:28 +0800

    add insert operation

--- src/List.java
+++ src/List.java
@@ -26,10 +26,44 @@ public class List {
         this._size = 0;
     }
 
+    private final Node nodeAtPos(final int index) {
+        Node temp = this._head;
+        int count = 0;
+        while (count < index && temp._next != null && temp._next != this._tail) {
+            temp = temp._next;
+            count++;
+        }
+        return temp;
+    }
+
     public List(final List other) {
         // Copy constructor
     }
 
+    public final void insert(final int index, final int data) {  // zero based index
+        Node temp = nodeAtPos(index);
+        Node newNode = new Node();
+        newNode._data = data;
+        newNode._prev = temp._next._prev;
+        newNode._next = temp._next;
+        temp._next = newNode;
+        newNode._next._prev = newNode;
+    }
+
+    public final void print() {
+        Node temp = this._head._next;
+        while (temp != this._tail) {
+            System.out.println(temp._data);
+            temp = temp._next;
+        }
+    }
+
     public static void main(String[] args) {
+        List myList = new List();
+        myList.insert(1, 0);
+        myList.insert(2, 1);
+        myList.insert(2, 4);
+        myList.insert(1, 10);
+        myList.print();
     }
 }
