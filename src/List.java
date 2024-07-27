24af7594a879184bc7b4d176e47db940fab0406c
Kurt Secretario <secretariokurt3219@gmail.com>
2024-07-27 11:40:18 +0800

    rebase node_class to main

--- src/List.java
+++ src/List.java
@@ -1,5 +1,15 @@
 public class List {
+    private class Node {
+        private Integer _data;
+        private Node _prev, _next;
+
+        public Node() {
+            this._data = null;
+            this._prev = null;
+            this._next = null;
+        }
+    }
+
     public static void main(String[] args) {
-        
     }
 }
